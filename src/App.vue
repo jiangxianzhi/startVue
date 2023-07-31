0b8dcad609a1f314d75dd7bf03bf1456cbd22188
jiangxianzhi <772423266@qq.com>
2023-07-31 08:53:55 +0800

    add v-on

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
-
-
 <template>
-
-
-<div>hello world</div>
+  <!-- <div>hello world</div>
 <router-link to="/egg">egg</router-link>
 <div></div>
 <router-link to="/apple">apple</router-link>
 <div></div>
 <router-link to ="/changeString">changeString</router-link>
-<div></div>
-<router-link v-for="item in items" :key="item.id" :to="`/${item.name}`">{{ items.name }}</router-link>
-<router-view></router-view>
-<router-view></router-view>
-
- 
-</template> 
+<div></div> -->
+  <!-- <router-link v-for="item in items" :key="item.id" :to="`/${item.name}`">{{ items.name }}</router-link> -->
+  <router-view></router-view>
+  <ShowItems />
+  <!-- 
+<ShowItems v-for="item in items" :key="item.id" :title="item.name"/> -->
+  <div :id="divId">{{ msg }}</div>
+  <h1 v-if="judgeAwsome">this is awsome</h1>
+  <button v-on:click="changeAwsome">changeAwsome</button>
+</template>
 
 <script>
 import items from '../item.json'
-export default{
-    data(){
-        return {
-            items
-        }
-    }
-}
+import ChangeString from './components/changeString.vue'
+import ShowItems from './components/showItems.vue'
 
+export default {
+  data() {
+    return {
+      items,
+      msg: 'abc',
+      divId: 'example1',
+      judgeAwsome: true
+    }
+  },
+  components: { ShowItems },
+  methods: {
+    changeAwsome() {
+      if (this.judgeAwsome) {
+        this.judgeAwsome = false
+        return
+      }
+      this.judgeAwsome = true
+    }
+  },
+  mounted() {
+    console.log('a')
+  }
+}
 </script>
+
+<style>
+#example1 {
+  background-color: rgb(109, 109, 223);
+}
+</style>
---
+++ src/components/showItems.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>hi</div>
+  <div>{{ itemsExist }}</div>
+</template>
+
+<script>
+import items from '../../item.json'
+
+export default {
+  data() {
+    return {
+      items
+    }
+  },
+  methods: {},
+  computed: {
+    itemsExist(){
+      return this.items.length>0?"yes":"no";
+    }
+  },
+  mounted(){
+    console.log(this.items);
+  }
+}
+</script>
+
+<style scoped>
+div {
+  color: rgb(71, 226, 97);
+}
+</style>
